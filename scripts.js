4717ced0dfd5d89fec23f8c53c4bc36260642a61
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-06-26 21:59:58 +0200

    using only 'reduce'

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
 // Part 1
+
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
 // using 'forEach' to console log each name:
 
@@ -10,7 +11,6 @@ names.forEach(name => {
 });
   
 // using 'forEach' to console log each name with a matching province:
-
 names.forEach((name, index) => {
     console.log(`${name} (${provinces[index]})`);
 });
@@ -28,13 +28,23 @@ const sortedProvinces = provinces.sort();
 console.log(sortedProvinces);
 
 //using 'filter' to remove provinces with the word "Cape" and returning the remaining count:
-
 const filteredProvinces = provinces.filter(province => !province.includes('Cape'));
 const remainingCount = filteredProvinces.length;
 
 console.log(remainingCount);
 
 //Using 'map' and 'some' to create a boolean array indicating wether a name contains an 'S' character:
-
 const hasSCharacter = names.map(name => name.includes('S'));
 console.log(hasSCharacter);
+
+
+//Using only reduce, turn the above into an object that indicates the province of an individual:
+const provinceObject = names.reduce((obj, name, index) => {
+    obj[name] = provinces[index];
+    return obj;
+  }, {});
+  
+  console.log(provinceObject);
+  
+
+  //Part 2
