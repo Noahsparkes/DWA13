3b82a3d9d2db9bbb644366f3516d1e6b99e30185
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-06-26 21:32:55 +0200

    initial file setup and 1st forEach log.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+
+// using 'forEach' to console log each name:
+
+names.forEach(name => {
+    console.log(name);
+  });
+  
+  
