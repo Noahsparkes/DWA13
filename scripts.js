ac8ef05204b6cc90f4e24830f6b04e72ee20caba
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-06-26 22:14:22 +0200

    readme adjustments.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//@ts-check
+
 // Part 1
 
 const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
@@ -58,3 +58,64 @@ const products = [
     { product: 'tea', price: '' },
   ]
 
+  console.log(
+    // Exercise 1: forEach to console.log each product name
+    (() => {
+      products.forEach(product => console.log(product.product));
+    })(),
+    // Exercise 2: filter out products with names longer than 5 characters
+    (() => {
+      return products.filter(product => product.product.length <= 5);
+    })(),
+    // Exercise 3: filter and map to convert prices and remove products without prices, then reduce to calculate the combined price
+    (() => {
+      return products
+        .filter(product => typeof product.price === 'number' && !isNaN(product.price))
+        .map(product => {
+          product.price = parseFloat(product.price);
+          return product;
+        })
+        .reduce((totalPrice, product) => totalPrice + product.price, 0);
+    })(),
+    // Exercise 4: reduce to concatenate all product names
+    (() => {
+      return products.reduce((result, product, index) => {
+        if (index === 0) {
+          return product.product;
+        } else if (index === products.length - 1) {
+          return `${result} and ${product.product}`;
+        } else {
+          return `${result}, ${product.product}`;
+        }
+      }, '');
+    })(),
+    // Exercise 5: reduce to calculate the highest and lowest-priced items
+    (() => {
+      const { highest, lowest } = products.reduce(
+        (result, product) => {
+          const price = typeof product.price === 'number' ? product.price : 0;
+          if (price > result.highest.price) {
+            result.highest = product;
+          }
+          if (price < result.lowest.price) {
+            result.lowest = product;
+          }
+          return result;
+        },
+        {
+          highest: { product: '', price: Number.NEGATIVE_INFINITY },
+          lowest: { product: '', price: Number.POSITIVE_INFINITY },
+        }
+      );
+      return `Highest: ${highest.product}. Lowest: ${lowest.product}.`;
+    })(),
+    // Exercise 6: Object.entries and reduce to recreate the object with modified keys
+    (() => {
+      return Object.entries(products).reduce((newObj, [key, value]) => {
+        const newKey = key === 'product' ? 'name' : key === 'price' ? 'cost' : key;
+        newObj[newKey] = value;
+        return newObj;
+      }, {});
+    })()
+  );
+  
